ac16c72e5214c064189884e97225a7e7d97f7660
Michael Hoevel <michael.hoevel.duisburg@gmail.com>
2016-12-15 22:13:38 +0100

    added bootstrap

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,13 @@
+class IndexController < ApplicationController
+  def about
+  end
+
+  def projects
+  end
+
+  def hobbies
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'index/about'
+
+  get 'index/projects'
+
+  get 'index/hobbies'
+
+  get 'index/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+   root to:"index#about"
 end
